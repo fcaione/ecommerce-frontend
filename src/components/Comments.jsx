ced92b45e9ed5d64499d308e6ac1d3969d6bb121
Freddy Caione <fac299@nyu.edu>
2023-03-06 15:36:57 -0500

    basic comment functionality

---
+++ src/components/Comments.jsx
@@ -0,0 +1,81 @@
+import { StarIcon } from "@heroicons/react/20/solid"
+
+const Comments = (props) => {
+
+	function classNames(...classes) {
+		return classes.filter(Boolean).join(" ")
+	}
+
+    console.log(props)
+
+	return (
+		<div className="bg-white">
+			<div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+				<h2 className="text-lg font-medium text-gray-900">
+					Recent comments
+				</h2>
+				<div className="mt-6 space-y-10 divide-y divide-gray-200 border-t border-b border-gray-200 pb-10">
+                    {console.log(props)}
+					{props.comments?.map((comment) => (
+						<div
+							key={comment.id}
+							className="pt-10 lg:grid lg:grid-cols-12 lg:gap-x-8"
+						>
+							<div className="lg:col-span-8 lg:col-start-5 xl:col-span-9 xl:col-start-4 xl:grid xl:grid-cols-3 xl:items-start xl:gap-x-8">
+								<div className="flex items-center xl:col-span-1">
+									<div className="flex items-center">
+										{[0, 1, 2, 3, 4].map((rating) => (
+											<StarIcon
+												key={rating}
+												className={classNames(
+													comment.rating > rating
+														? "text-yellow-400"
+														: "text-gray-200",
+													"h-5 w-5 flex-shrink-0"
+												)}
+												aria-hidden="true"
+											/>
+										))}
+									</div>
+									<p className="ml-3 text-sm text-gray-700">
+										{comment.rating}
+										<span className="sr-only">
+											{" "}
+											out of 5 stars
+										</span>
+									</p>
+								</div>
+
+								<div className="mt-4 lg:mt-6 xl:col-span-2 xl:mt-0">
+									<h3 className="text-sm font-medium text-gray-900">
+										{comment.title}
+									</h3>
+
+									<div
+										className="mt-3 space-y-6 text-sm text-gray-500"
+										dangerouslySetInnerHTML={{
+											__html: comment.content,
+										}}
+									/>
+								</div>
+							</div>
+
+							<div className="mt-6 flex items-center text-sm lg:col-span-4 lg:col-start-1 lg:row-start-1 lg:mt-0 lg:flex-col lg:items-start xl:col-span-3">
+								<p className="font-medium text-gray-900">
+									{comment.author}
+								</p>
+								<time
+									dateTime={comment.datetime}
+									className="ml-4 border-l border-gray-200 pl-4 text-gray-500 lg:ml-0 lg:mt-2 lg:border-0 lg:pl-0"
+								>
+									{comment.date}
+								</time>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	)
+}
+export default Comments
--- src/pages/ListDetails.jsx
+++ src/pages/ListDetails.jsx
@@ -1,113 +1,133 @@
-import { useState, useEffect } from 'react'
-import { Disclosure, RadioGroup, Tab } from '@headlessui/react'
-import { StarIcon } from '@heroicons/react/20/solid'
-import { HeartIcon, MinusIcon, PlusIcon } from '@heroicons/react/24/outline'
-import Client from '../services/api'
-import { useParams } from 'react-router-dom'
+import { useState, useEffect } from "react"
+import { Disclosure, RadioGroup, Tab } from "@headlessui/react"
+import { StarIcon } from "@heroicons/react/20/solid"
+import { HeartIcon, MinusIcon, PlusIcon } from "@heroicons/react/24/outline"
+import Client from "../services/api"
+import { useParams } from "react-router-dom"
+import Comments from "../components/Comments"
 
 const ListDetails = () => {
+	function classNames(...classes) {
+		return classes.filter(Boolean).join(" ")
+	}
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
+	let { listingId } = useParams()
 
-  let { listingId } = useParams()
+	// const [selectedColor, setSelectedColor] = useState(product.colors[0])
 
-  // const [selectedColor, setSelectedColor] = useState(product.colors[0])
+	const [selectedListing, setSelectedListing] = useState({})
 
-  const [selectedListing, setSelectedListing] = useState({})
+	const getListing = async () => {
+		const res = await Client.get(`/listings/${listingId}`)
+		console.log(res)
+		setSelectedListing(res.data)
+	}
 
-  const getListing = async () => {
-    const res = await Client.get(`/listings/${listingId}`)
-    console.log(res)
-    setSelectedListing(res.data)
-  }
+	useEffect(() => {
+		getListing()
+	}, [])
 
-  useEffect(() => {
-    getListing()
-  }, [])
+	return (
+		<>
+			<div className="bg-white">
+				<div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+					<div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+						{/* Image gallery */}
+						<div className="h-56 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-72 xl:h-80">
+							<img
+								src={selectedListing.image}
+								alt="picture of product"
+								className="h-full w-full object-cover object-center"
+							/>
+						</div>
 
-  return (
-    <div className="bg-white">
-      <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-          {/* Image gallery */}
-          <div className="h-56 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-72 xl:h-80">
-					<img
-						src={selectedListing.image}
-						alt="picture of product"
-						className="h-full w-full object-cover object-center"
-					/>
-          </div>
+						{/* Product info */}
+						<div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+							<h1 className="text-3xl font-bold tracking-tight text-gray-900">
+								{selectedListing.name}
+							</h1>
 
-          {/* Product info */}
-          <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">{selectedListing.name}</h1>
+							<div className="mt-3">
+								<h2 className="sr-only">Product information</h2>
+								<p className="text-3xl tracking-tight text-gray-900">
+									${selectedListing.price}
+								</p>
+							</div>
 
-            <div className="mt-3">
-              <h2 className="sr-only">Product information</h2>
-              <p className="text-3xl tracking-tight text-gray-900">${selectedListing.price}</p>
-            </div>
+							{/* Reviews */}
+							<div className="mt-3">
+								<h3 className="sr-only">Reviews</h3>
+								<div className="flex items-center">
+									<div className="flex items-center">
+										{[0, 1, 2, 3, 4].map((rating) => (
+											<StarIcon
+												key={rating}
+												className={classNames(
+													selectedListing.rating >
+														rating
+														? "text-indigo-500"
+														: "text-gray-300",
+													"h-5 w-5 flex-shrink-0"
+												)}
+												aria-hidden="true"
+											/>
+										))}
+									</div>
+									<p className="sr-only">
+										{selectedListing.rating} out of 5 stars
+									</p>
+								</div>
+							</div>
 
-            {/* Reviews */}
-            <div className="mt-3">
-              <h3 className="sr-only">Reviews</h3>
-              <div className="flex items-center">
-                <div className="flex items-center">
-                  {[0, 1, 2, 3, 4].map((rating) => (
-                    <StarIcon
-                      key={rating}
-                      className={classNames(
-                        selectedListing.rating > rating ? 'text-indigo-500' : 'text-gray-300',
-                        'h-5 w-5 flex-shrink-0'
-                      )}
-                      aria-hidden="true"
-                    />
-                  ))}
-                </div>
-                <p className="sr-only">{selectedListing.rating} out of 5 stars</p>
-              </div>
-            </div>
+							<div className="mt-6">
+								<h3 className="sr-only">Description</h3>
 
-            <div className="mt-6">
-              <h3 className="sr-only">Description</h3>
+								<div
+									className="space-y-6 text-base text-gray-700"
+									dangerouslySetInnerHTML={{
+										__html: selectedListing.description,
+									}}
+								/>
+							</div>
 
-              <div
-                className="space-y-6 text-base text-gray-700"
-                dangerouslySetInnerHTML={{ __html: selectedListing.description }}
-              />
-            </div>
-
-            <form className="mt-6">
-              {/* Colors */}
-              {/* <div>
+							<form className="mt-6">
+								{/* Colors */}
+								{/* <div>
                 <h3 className="text-sm text-gray-600">Color</h3>
               </div> */}
 
-              <div className="sm:flex-col1 mt-10 flex">
-                <button
-                  type="submit"
-                  className="flex max-w-xs flex-1 items-center justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50 sm:w-full"
-                >
-                  Add to bag
-                </button>
+								<div className="sm:flex-col1 mt-10 flex">
+									<button
+										type="submit"
+										className="flex max-w-xs flex-1 items-center justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50 sm:w-full"
+									>
+										Add to bag
+									</button>
 
-                <button
-                  type="button"
-                  className="ml-4 flex items-center justify-center rounded-md py-3 px-3 text-gray-400 hover:bg-gray-100 hover:text-gray-500"
-                >
-                  <HeartIcon className="h-6 w-6 flex-shrink-0" aria-hidden="true" />
-                  <span className="sr-only">Add to favorites</span>
-                </button>
-              </div>
-            </form>
+									<button
+										type="button"
+										className="ml-4 flex items-center justify-center rounded-md py-3 px-3 text-gray-400 hover:bg-gray-100 hover:text-gray-500"
+									>
+										<HeartIcon
+											className="h-6 w-6 flex-shrink-0"
+											aria-hidden="true"
+										/>
+										<span className="sr-only">
+											Add to favorites
+										</span>
+									</button>
+								</div>
+							</form>
 
-            <section aria-labelledby="details-heading" className="mt-12">
-              <h2 id="details-heading" className="sr-only">
-                Additional details
-              </h2>
+							<section
+								aria-labelledby="details-heading"
+								className="mt-12"
+							>
+								<h2 id="details-heading" className="sr-only">
+									Additional details
+								</h2>
 
-              {/* <div className="divide-y divide-gray-200 border-t">
+								{/* <div className="divide-y divide-gray-200 border-t">
                 {selectedListing.details.map((detail) => (
                   <Disclosure as="div" key={detail.name}>
                     {({ open }) => (
@@ -146,11 +166,13 @@ function classNames(...classes) {
                   </Disclosure>
                 ))}
               </div> */}
-            </section>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+							</section>
+						</div>
+					</div>
+				</div>
+			</div>
+			<Comments comments={selectedListing.comments}/>
+		</>
+	)
 }
 export default ListDetails
