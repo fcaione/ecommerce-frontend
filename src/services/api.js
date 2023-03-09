b2932d90381e853775ccf4f121510ca65dd7d78d
Freddy Caione <fac299@nyu.edu>
2023-03-09 11:10:25 -0500

    BASE_URL in api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
 
-export const BASE_URL = 'http://localhost:3001/api'
+export const BASE_URL = 'https://ecommerce-database.herokuapp.com/api'
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
