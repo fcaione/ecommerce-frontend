67151ff12661b903b2b03d344c42e82f146b448a
Freddy Caione <fac299@nyu.edu>
2023-03-09 11:51:13 -0500

    changed a tag to link in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -229,10 +229,10 @@ const Header = ({ user, setUser }) => {
                               <div className="aspect-w-1 aspect-h-1 overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75">
                                 <img src={item.imageSrc} alt={item.imageAlt} className="object-cover object-center" />
                               </div>
-                              <a href={item.href} className="mt-6 block text-sm font-medium text-gray-900">
+                              <Link to={item.href} className="mt-6 block text-sm font-medium text-gray-900">
                                 <span className="absolute inset-0 z-10" aria-hidden="true" />
                                 {item.name}
-                              </a>
+                              </Link>
                               <p aria-hidden="true" className="mt-1 text-sm text-gray-500">
                                 Shop now
                               </p>
