20189901b8a01ef1a028374fae4bc6320c48ea23
Freddy Caione <fac299@nyu.edu>
2023-03-09 15:52:10 -0500

    hero and highlight text changed

--- src/components/Highlight.jsx
+++ src/components/Highlight.jsx
@@ -1,4 +1,5 @@
 import posingshoes from '../images/posingshoes.webp'
+import { Link } from 'react-router-dom'
 const Highlight = () => {
 
   return (
@@ -14,19 +15,17 @@ const Highlight = () => {
             <div className="relative bg-gray-900 bg-opacity-75 py-32 px-6 sm:py-40 sm:px-12 lg:px-16">
               <div className="relative mx-auto flex max-w-3xl flex-col items-center text-center">
                 <h2 id="comfort-heading" className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                  Simple productivity
+                  Reuseability
                 </h2>
                 <p className="mt-3 text-xl text-white">
-                  Endless tasks, limited hours, a single piece of paper. Not really a haiku, but we're doing our best
-                  here. No kanban boards, burndown charts, or tangled flowcharts with our Focus system. Just the
-                  undeniable urge to fill empty circles.
+                Passed down fashion holds significant cultural and historical value as it tells the story of previous generations and their unique styles. By embracing and preserving these pieces, we not only honor our past but also contribute to a sustainable and environmentally conscious future.
                 </p>
-                <a
+                {/* <Link
                   href="#"
                   className="mt-8 block w-full rounded-md border border-transparent bg-white py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-100 sm:w-auto"
                 >
                   Shop Focus
-                </a>
+                </Link> */}
               </div>
             </div>
           </div>
